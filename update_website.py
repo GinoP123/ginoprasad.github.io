4a73c9e3e97bdaff6135833101783cd3ed59f361
Gino Prasad <ginoprasad@gmail.com>
2023-07-25 19:55:07 -0700

    Automated Website Update

--- update_website.py
+++ update_website.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[17]:
 
 
 import os
 import subprocess as sp
 import yaml
 import shutil
+from tqdm import tqdm
 
 
 # # Convert notebooks to html
@@ -61,37 +62,41 @@ temp_path
 project_notebook_path
 
 
-# In[ ]:
+# In[35]:
 
 
 project_names, project_paths = [], []
-for project_notebook_path in metadata['Projects']:
-    print(f"Converting {project_notebook_path}")
-    sp.run(f"jupyter nbconvert --to html '{project_notebook_path}' --output '{temp_path}'", shell=True)
+for project_notebook_path in tqdm(metadata['Projects']):
+    title_line = sp.run(f"head -n 200 '{project_notebook_path}'", shell=True, capture_output=True).stdout.decode().split('\n')
+    title_line = title_line[title_line.index('   "source": [') + 1]
+    project_name = title_line.strip().lstrip('"# ').rstrip('\\n",').strip()
     
-    with open(temp_path) as infile:
-        lines = infile.readlines()
-
-    title_line = sp.run(f"grep '<h1' '{temp_path}'", shell=True, capture_output=True).stdout.decode().split('\n')[0]
-    project_name = title_line[title_line.index('>')+1:]
-    project_name = project_name[:project_name.index('<')]
-    print(f'Project Name: {project_name}')
-
     project_base_path = os.path.basename(project_notebook_path)[:-len('.ipynb')]
     while len(project_base_path) > max_base_filename_length:
         project_base_path = ' '.join(project_base_path.split(' ')[:-1])
+    if not project_base_path:
+        print(f"\n\n\n\n\tWarning: Project '{project_name}' Name exceeds recommended length\n\n\n\n")
+        project_base_path = project_name
+    project_path = f'{os.getcwd()}/projects/{project_base_path}.html'
+    
+    if os.path.getmtime(project_path) > os.path.getmtime(project_notebook_path):
+        continue
+    
+    print(project_base_path)
+    print(f"Converting {project_notebook_path}")
+    sp.run(f"jupyter nbconvert --to html '{project_notebook_path}' --output '{temp_path}'", shell=True)
+    print(f'Project Name: {project_name}')
+
+    with open(temp_path) as infile:
+        lines = infile.readlines()
+
+
     title = ' '.join(map(lambda x: x[0].upper() + x[1:] if x else x, project_base_path.split('_')))
     lines[5] = lines[5][:len('<title>')] + title + lines[5][lines[5].index('</title>'):]
  
     with open(temp_path, 'w') as outfile:
         lines.insert(5, '<link rel="icon" href="../docs/assets/logo.png"><iframe src="../header.html" style="height: 12rem; width: 100%" frameborder="0" scrolling="no"></iframe>\n')
         outfile.write(''.join(lines))
-    
-    if not project_base_path:
-        print(f"\n\n\n\n\tWarning: Project '{project_name}' Name exceeds recommended length\n\n\n\n")
-        project_base_path = project_name
-    print(f'Project Path: {project_base_path}')
-    project_path = f'{os.getcwd()}/projects/{project_base_path}.html'
     
     assert project_path not in project_paths
     os.rename(temp_path, project_path)
@@ -101,14 +106,14 @@ for project_notebook_path in metadata['Projects']:
     print('\n')
 
 
-# In[ ]:
+# In[10]:
 
 
 index_html_path = 'index.html'
 index_html_lines = open(index_html_path).readlines()
 
 
-# In[ ]:
+# In[11]:
 
 
 publications_list_index_start = ["Publications" in x for x in index_html_lines].index(True) + 2
@@ -121,7 +126,7 @@ for publication in metadata['Publications']:
 index_html_lines = index_html_lines[:publications_list_index_start] + publications_list + index_html_lines[publications_list_index_end:]
 
 
-# In[ ]:
+# In[12]:
 
 
 project_list_index_start = ["Cool Projects" in x for x in index_html_lines].index(True) + 2
@@ -134,20 +139,20 @@ index_html_lines[project_list_index_start-2] = f"\t\t<h2> Cool Projects ({len(me
 
 # # Copying CV and Updating Links
 
-# In[ ]:
+# In[13]:
 
 
 assert shutil.copy(metadata['CV'], f"projects/{os.path.basename(metadata['CV'])}")
 
 
-# In[ ]:
+# In[14]:
 
 
 tag_dict = {tag: metadata[tag] for tag in ['CV', 'LinkedIn', 'GitHub']}
 tag_dict['CV'] = f"projects/{os.path.basename(tag_dict['CV'])}"
 
 
-# In[ ]:
+# In[15]:
 
 
 for i, line in enumerate(index_html_lines):
@@ -165,14 +170,14 @@ for i, line in enumerate(index_html_lines):
 
 # # Writing Updated Index File
 
-# In[ ]:
+# In[16]:
 
 
 with open(index_html_path, 'w') as outfile:
     outfile.write(''.join(index_html_lines))
 
 
-# In[ ]:
+# In[17]:
 
 
 sp.run(f"cd '{os.getcwd()}'; git add .; git commit -m 'Automated Website Update'; git push origin main", shell=True)
@@ -180,7 +185,7 @@ sp.run(f"cd '{os.getcwd()}'; git add .; git commit -m 'Automated Website Update'
 
 # # Updating Python Script
 
-# In[ ]:
+# In[18]:
 
 
 if hasattr(__builtins__,'__IPYTHON__'):
